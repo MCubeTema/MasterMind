d09c6a37e4a402720ad99e439d105f5a040fb95c
AnselmTheCoder <omoyano97@gmail.com>
2017-11-07 15:47:47 +0100

    Project structure
    An initial structure for our Java code. May have some minor problems and changes on the future.

---
+++ MasterMind/src/java/main/Domain/DomainController.java
@@ -0,0 +1,5 @@
+package java.main.Domain;
+
+public class DomainController {
+//First class to implement (at least on pseudocode) 
+}
---
+++ MasterMind/src/java/main/main.java
@@ -0,0 +1,6 @@
+package java.main;
+
+public class main {
+//Class without any code, just used a placeholder for future
+//expansion of the code
+}
