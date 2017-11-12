4577fd80d717a3f4a9562f1bb54dcbd13fdc7b8a
SrManny <32618674+SrManny@users.noreply.github.com>
2017-11-12 19:44:53 +0100

    Modo Ahora si acabado

---
+++ MasterMind/src/mcubetema/domain/Mode.java
@@ -0,0 +1,80 @@
+package mcubetema.domain;
+
+public class Mode {
+	
+	private String type;
+	private Double difFactor;
+	private Double clueFactor;
+	private Double rowBon;
+	private Integer numberColors;
+	private Double nRows;
+	private Integer nCols;
+	
+	public Mode() {
+		
+	}
+	public Mode(String t) {
+		type = t;
+		if (type.equals("Easy")) {
+			setEasy();
+		}
+		else if (type.equals("Medium")) {
+			setMedium();
+		}
+		else if(type.equals("Difficult")) {
+			setDifficult();
+		}
+	}
+	
+	public void setDifficult() {
+		difFactor = 1.25;
+		clueFactor = 50.0;
+		rowBon = 125.0;
+		numberColors = 8;
+		nRows = 12.;
+		nCols = 5;
+	}
+	public void setMedium() {
+		difFactor = 1.0;
+		clueFactor = 35.0;
+		rowBon = 100.0;
+		numberColors = 5;
+		nRows = 10.;
+		nCols = 4; 
+	}
+	public void setEasy() {
+		difFactor = 0.75;
+		clueFactor = 20.0;
+		rowBon = 100.0;
+		numberColors = 5;
+		nRows = 8.;
+		nCols = 4;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	public Double getdifFactor() {
+		return difFactor;
+	}
+	
+	public Double getclueFactor() {
+		return clueFactor;
+	}
+	
+	public Double getrowBon() {
+		return rowBon;
+	}
+	
+	public Integer getnumberColors() {
+		return numberColors;
+	}
+	
+	public Double getnRows() {
+		return nRows;
+	}
+	
+	public Integer getnCols() {
+		return nCols;
+	}
+}
