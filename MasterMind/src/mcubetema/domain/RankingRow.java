0f1e81843292e8c86d206c50f9cec10a1f24b700
SrManny <32618674+SrManny@users.noreply.github.com>
2017-11-16 13:00:47 +0100

    RankingRowComentado

--- MasterMind/src/mcubetema/domain/RankingRow.java
+++ MasterMind/src/mcubetema/domain/RankingRow.java
@@ -7,7 +7,7 @@ public class RankingRow implements Comparable<RankingRow>{
 	
 	public RankingRow() {
 	}
-	
+	/*Create a new RankingRow with user u and score s*/
 	public RankingRow(String u, Integer s) {
 		user = u;
 		score = s;
@@ -27,6 +27,7 @@ public class RankingRow implements Comparable<RankingRow>{
 	}
 	
 	@Override
+	/*Compare a RankingRow with another RankingRow, first by score and then by UserName*/
 	public int compareTo(RankingRow r1) {
 		if (score < r1.score) {
 			return 1;
